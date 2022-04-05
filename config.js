547c31ac1a6ffd4bb4a0d7e1ed1b3be4c1878869
BenzzTempest <103017549+BenzzTempest@users.noreply.github.com>
2022-04-05 11:39:06 +0700

    config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','6281382420312','6288292024190']
-global.premium = ['6288292024190']
+global.owner = ['6283167714830','6285721055362','6281382420312','6288292024190']
+global.premium = ['6285721055362']
 global.packname = 'Hisoka Morrow'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
